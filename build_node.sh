b9c2e6a9aaec543c77e7f3510dfff1c6a6386512
Eyal Lotem <eyal.lotem@gmail.com>
2016-06-22 17:03:19 +0300

    Relax constraint on nodejs 6.2.2 -> 6.2.1

--- build_node.sh
+++ build_node.sh
@@ -3,11 +3,10 @@
 # adapted from https://apple.stackexchange.com/a/123408/11374
 function version { echo "${@//v}" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
-NODE_VERSION_STRING=`node --version`
-NODE_VERSION=$(version $NODE_VERSION_STRING)
+NODE_VERSION=`node --version`
 
-if [ "$NODE_VERSION" -ge $(version "v6.2.2") ]; then
-    echo "Using existing system node $NODE_VERSION_STRING"
+if [ $(version "$NODE_VERSION") -ge $(version "v6.2.1") ]; then
+    echo "Using existing system node $NODE_VERSION"
     cp `which node` node/node
 else
     echo "Building nodejs from source"
